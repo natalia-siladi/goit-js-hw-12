1eb3781c8224eb5347e7b326d266fac5bbe5565a
natalia-siladi <80765980+natalia-siladi@users.noreply.github.com>
2022-08-23 07:12:49 +0000

    Deploying to gh-pages from @ natalia-siladi/goit-js-hw-12@817a45536a3a33ab496e9fa675277b9c67fa6d79 🚀

---
+++ index.3354723d.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/{name}");
+//# sourceMappingURL=index.3354723d.js.map
---
+++ index.e29e6908.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/{name}");
+//# sourceMappingURL=index.e29e6908.js.map
